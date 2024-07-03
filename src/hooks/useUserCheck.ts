3852754a3d9673cd663ce7d146a0fa970d8ac2bd
Hassan Raza <hassanraza01.dev@gmail.com>
2024-07-03 13:05:18 -0500

    Update eslint and tailwind config

--- src/hooks/useUserCheck.ts
+++ src/hooks/useUserCheck.ts
@@ -1,27 +1,27 @@
-import { Player } from "@prisma/client";
-import { useState, useEffect } from "react";
+import { Player } from '@prisma/client'
+import { useState, useEffect } from 'react'
 
 const useUserCheck = () => {
-  const [userExists, setUserExists] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [player, setPlayer] = useState<Player>();
+  const [userExists, setUserExists] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [player, setPlayer] = useState<Player>()
 
   useEffect(() => {
     try {
-      const player = JSON.parse(localStorage.getItem('player') || '');
+      const player = JSON.parse(localStorage.getItem('player') || '')
 
       if(player) {
-        setUserExists(true);
-        setPlayer(player);
+        setUserExists(true)
+        setPlayer(player)
       }
     } catch (error) {
-      console.error('Error parsing player from local storage:', error);
+      console.error('Error parsing player from local storage:', error)
     }
     
-    setLoading(false);
+    setLoading(false)
   }, [])
 
-  return { userExists, loading, setUserExists, player };
+  return { userExists, loading, setUserExists, player }
 }
 
-export default useUserCheck;
+export default useUserCheck
